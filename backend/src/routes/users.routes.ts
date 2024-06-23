66eaa425acc763bad2af0c834a54b3149d3ac708
Satyaranjan Pattanaik <srpattanaik.1702@gmail.com>
2024-06-23 19:30:38 +0530

    add User Registration Form

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -40,7 +40,7 @@ router.post("/register", [
             secure: process.env.NODE_ENV === "production",
             maxAge: 86400000,
         })
-        return res.sendStatus(200);
+        return res.status(200).send({ message: "User register OK" });
 
 
 
---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,120 @@
+import { useForm } from "react-hook-form";
+import { useMutation } from "react-query";
+import * as apiClient from "../api-client";
+
+export type registerFormData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmedPassword: string;
+};
+
+const Register = () => {
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<registerFormData>();
+
+  const mutation = useMutation(apiClient.register, {
+    onSuccess: () => {
+      console.log("registration successfull");
+    },
+
+    onError: (error: Error) => {
+      console.log(error.message);
+    },
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    mutation.mutate(data);
+  });
+  return (
+    <form className="flex flex-col gap-5" onSubmit={onSubmit}>
+      <h2 className="text-3xl font-bold">Create an Account</h2>
+      <div className="flex flex-col gap-5 md:flex-row">
+        <label className="flex-1 text-sm font-bold text-gray-700">
+          First Name
+          <input
+            className="w-full px-2 py-1 font-normal border rounded "
+            {...register("firstName", { required: "This field is required" })}
+          ></input>
+          {errors.firstName && (
+            <span className="text-red-500">{errors.firstName.message}</span>
+          )}
+        </label>
+        <label className="flex-1 text-sm font-bold text-gray-700">
+          Last Name
+          <input
+            className="w-full px-2 py-1 font-normal border rounded "
+            {...register("lastName", { required: "This field is required" })}
+          ></input>
+          {errors.lastName && (
+            <span className="text-red-500">{errors.lastName.message}</span>
+          )}
+        </label>
+      </div>
+      <label className="flex-1 text-sm font-bold text-gray-700">
+        Email
+        <input
+          type="email"
+          className="w-full px-2 py-1 font-normal border rounded "
+          {...register("email", { required: "This field is required" })}
+        ></input>
+        {errors.email && (
+          <span className="text-red-500">{errors.email.message}</span>
+        )}
+      </label>
+      <label className="flex-1 text-sm font-bold text-gray-700">
+        Password
+        <input
+          type="password"
+          className="w-full px-2 py-1 font-normal border rounded "
+          {...register("password", {
+            required: "This field is required",
+            minLength: {
+              value: 6,
+              message: "The password must be six characters",
+            },
+          })}
+        ></input>
+        {errors.password && (
+          <span className="text-red-500">{errors.password.message}</span>
+        )}
+      </label>
+      <label className="flex-1 text-sm font-bold text-gray-700">
+        Confirm Password
+        <input
+          type="password"
+          className="w-full px-2 py-1 font-normal border rounded "
+          {...register("confirmedPassword", {
+            validate: (val) => {
+              if (!val) {
+                return "This field is required";
+              } else if (watch("password") !== val) {
+                return "Your password do not match";
+              }
+            },
+          })}
+        ></input>
+        {errors.confirmedPassword && (
+          <span className="text-red-500">
+            {errors.confirmedPassword.message}
+          </span>
+        )}
+      </label>
+      <span>
+        <button
+          type="submit"
+          className="p-2 font-bold text-white bg-blue-600 hover:bg-blue-500"
+        >
+          {" "}
+          Create Account
+        </button>
+      </span>
+    </form>
+  );
+};
+export default Register;
