d18af0a7c00a2652cabb3d2829130d258e23347c
Satyaranjan Pattanaik <srpattanaik.1702@gmail.com>
2024-06-25 17:13:05 +0530

    add toast Notification

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,6 +1,8 @@
 import { useForm } from "react-hook-form";
 import { useMutation } from "react-query";
 import * as apiClient from "../api-client";
+import { useAppContext } from "../contexts/AppContext";
+import { useNavigate } from "react-router-dom";
 
 export type registerFormData = {
   firstName: string;
@@ -11,6 +13,8 @@ export type registerFormData = {
 };
 
 const Register = () => {
+  const navigate = useNavigate();
+  const { showToast } = useAppContext();
   const {
     register,
     watch,
@@ -20,11 +24,12 @@ const Register = () => {
 
   const mutation = useMutation(apiClient.register, {
     onSuccess: () => {
-      console.log("registration successfull");
+      showToast({ message: "Registration Sucess!", type: "SUCCESS" });
+      navigate("/");
     },
 
     onError: (error: Error) => {
-      console.log(error.message);
+      showToast({ message: error.message, type: "ERROR" });
     },
   });
 
